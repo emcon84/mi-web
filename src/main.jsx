74121ea41520569abbdd338bbe73a2ad5c4baf9f
emiliano <emcon84@gmail.com>
2025-05-07 12:10:59 -0300

    update

--- src/main.jsx
+++ src/main.jsx
@@ -7,17 +7,16 @@ import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <div className="fixed top-[87%] z-50 w-full flex justify-center flex-col items-center">
-      <p>Contactame</p>
+    <div className="fixed top-10 z-50 w-full flex justify-center flex-col items-center">      
       <ul className="py-3 flex justify-center w-full">
         <li>
           <a href="mailto:emcon84@gmail.com" target="_blank">
-            <MdEmail className="text-white mr-10" size={24} />
+            <MdEmail className="text-gray-500 mr-10" size={34} />
           </a>
         </li>        
         <li>
           <a href="https://www.linkedin.com/in/emiliano-conti/" target="_blank">
-            <IoLogoLinkedin className="text-white" size={24} />
+            <IoLogoLinkedin className="text-gray-500" size={34} />
           </a>
         </li>
       </ul>
